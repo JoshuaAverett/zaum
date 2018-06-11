d555a2c47f8ed6fc0033eddca2302179d9e72118
qonfluent <>
2018-06-11 01:13:13 -0400

    Changes the name of the build directory to '.build'

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 set -e
 	compile_file () {
-		clang -c -g -std=gnu11 -I "include" "src/$1.c" -o "output/$1.o" -Weverything
+		clang -c -g -std=gnu11 -I "include" "src/$1.c" -o ".build/$1.o" -Weverything
 	}
 
-	mkdir -p output
+	mkdir -p .build
 
 	compile_file "test-main"
 	compile_file "test"
 
-	cd output
+	cd .build
 		clang -o test \
 			test-main.o \
 			test.o
